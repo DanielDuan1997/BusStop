d7795c3b856c4338277a3c3844de7086e75352ab
niabbf <duanlei601@163.com>
2019-05-27 16:41:01 +0800

    Finished

--- backend/app.py
+++ backend/app.py
@@ -19,13 +19,15 @@ def get_schedule():
     post_data = request.form.to_dict()
     place_from = post_data.get("from")
     place_to = post_data.get("to")
-    start_dt = datetime.strptime(post_data.get("datetime"), "%Y-%m-%d")
+    start_date = datetime.strptime(post_data.get("date"), "%Y-%m-%d")
 
-    sp_check = get_special_days(start_dt)
+    sp_check = get_special_days(start_date)
     if sp_check != None:
         return make_response(jsonify({'schedule': sp_check}), 200)
 
-    weekday = start_dt.weekday()
+    weekday = start_date.weekday()
+    notice = ""
+    schedule = []
     if place_from == r"张江":
         if place_to == r"邯郸": # ZhangJiang -> HanDan
             notice = r"Ⅰ期图书馆西侧（近足球场一侧人行道）"
@@ -41,9 +43,12 @@ def get_schedule():
                 schedule = ['7:50', '12:15', '15:00', '17:00', '19:00']
             else:
                 schedule = []
-        else:
-            notice = ""
-            schedule = []
+        elif place_to == r"上科大":
+            notice = r"图书馆西侧（近足球场一侧人行道）"
+            if weekday < 5:
+                schedule = ['12:30', '18:00', '21:30', '22:00']
+            else:
+                schedule = ['17:20', '21:00']
     elif place_from == r"邯郸":
         if place_to == r"张江": # HanDan -> ZhangJiang
             notice = r"日月西路近理科图书馆"
@@ -74,9 +79,6 @@ def get_schedule():
                             '17:10', '18:00', '19:30', '20:20', '21:25']
             else:
                 schedule = ['08:00', '16:30']
-        else:
-            notice = ""
-            schedule = []
     elif place_from == r"枫林":
         if place_to == r"邯郸": # FengLin -> HanDan
             notice = r"西苑（近8号楼）"
@@ -92,11 +94,6 @@ def get_schedule():
             if weekday < 5:
                 schedule = ['07:00', '07:30', '09:00', '12:15', '13:30', '16:30',
                             '19:00']
-            else:
-                schedule = []
-        else:
-            notice = ""
-            schedule = []
     elif place_from == r"江湾":
         if place_to == r"邯郸": # JiangWan -> HanDan
             notice = r"校区宣传栏"
@@ -111,12 +108,15 @@ def get_schedule():
             else:
                 schedule = ['07:40', '08:00', '08:20', '08:40', '09:00', '09:20',
                             '09:40', '10:20', '17:30', '20:40', '21:20', '22:10']
-        else:
-            notice = ""
-            schedule = []
-    else:
-        schedule = []
+    elif place_from == r"上科大":
+        if place_to == r"张江": # ShangKeDa -> ZhangJiang
+            notice = r"上科大学生宿舍区内"
+            if weekday < 5:
+                schedule = ['07:30', '08:00', '08:30', '09:00', '13:00', '14:15', '17:30']
+            else:
+                schedule = ['09:00', '12:00', '17:00']
     return make_response(jsonify({'schedule': schedule, 'notice': notice}), 200)
 
 if __name__ == '__main__':
+    app.config['JSON_AS_ASCII'] = False
     app.run(host='0.0.0.0', debug=True, port=5001)
--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -2,8 +2,17 @@
   <mu-container class="center">
     <mylogo></mylogo>
     <myselector :from.sync="from" :to.sync="to" :start-date.sync="startDate" :submit="submit"></myselector>
-    <mu-list v-if="!this.err">
-    </mu-list>
+    <div v-if="!err" class="main-area">
+      <div class="bus-title">{{data.notice}}</div>
+      <div v-if="data.schedule.length">
+        <div v-for="(bus, index) in data.schedule" :class="getStyle(data.style[index])">
+          <span class="bus-text">{{bus}}: &nbsp; {{ori_from}} &nbsp; -> &nbsp; {{ori_to}}</span>
+        </div>
+      </div>
+      <div v-else>
+        当日无校车
+      </div>
+    </div>
     <div v-else class="error-text">
       无法连接到服务器
     </div>
@@ -18,9 +27,15 @@ export default {
     return {
       from: '张江',
       to: '邯郸',
+      ori_from: '张江',
+      ori_to: '邯郸',
       startDate: '',
       err: false,
+      data: {'notice': undefined, 'schedule': []}
     }
+  },
+  created: function () {
+    this.submit()
   },
   methods: {
     submit () {
@@ -29,8 +44,8 @@ export default {
         this.startDate = curDate.getFullYear() + '-' + (curDate.getMonth() + 1) + '-' + curDate.getDate()
       }
       let formData = new FormData()
-      formData.append('from', this.from[0])
-      formData.append('to', this.to[0])
+      formData.append('from', this.from)
+      formData.append('to', this.to)
       formData.append('date', this.startDate)
       let opts = {
         head: {'content-type' : 'application/x-www-form-urlencoded'},
@@ -38,11 +53,44 @@ export default {
         body: formData
       }
       fetch('http://0.0.0.0:5001/', opts)
-        .then(response => this.callback(response.text()))
+        .then(response => response.text())
+        .then(text => this.callback(text))
         .catch(response => {this.err = true})
     },
-    callback(response) {
-      console.log(response)
+    sameDate (date_1, date_2) {
+      return date_1.getFullYear() == date_2.getFullYear()
+          && date_1.getMonth() == date_2.getMonth()
+          && date_1.getDate() == date_2.getDate()
+    },
+    callback (text) {
+      this.ori_from = this.from
+      this.ori_to = this.to
+      this.data = JSON.parse(text)
+      let length = this.data.schedule.length
+      this.data.style = new Array(length)
+      let curTime = new Date()
+      let busDate = this.startDate.split('-')
+      let curBusTime = new Date()
+      curBusTime.setFullYear(Number(busDate[0]), Number(busDate[1]) - 1, Number(busDate[2]))
+      let sameDate = this.sameDate(curTime, curBusTime)
+      for (let i=0; i<length; i++) {
+        let busTime = this.data.schedule[i].split(':')
+        curBusTime.setHours(Number(busTime[0]), Number(busTime[1]))
+        if (curBusTime < curTime)
+          this.data.style[i] = 0
+        else if (sameDate && (i == 0 || this.data.style[i - 1] == 0))
+          this.data.style[i] = 1
+        else
+          this.data.style[i] = 2
+      }
+    },
+    getStyle (tp) {
+      if (tp == 0)
+        return "bulletin0"
+      else if (tp == 1)
+        return "bulletin1"
+      else
+        return "bulletin2"
     }
   }
 };
@@ -52,6 +100,50 @@ export default {
 .center {
   text-align: center;
   padding: 10px;
+}
+.main-area {
+  margin-top: 10px;
+  z-index: -1;
+}
+.bulletin0 {
+  background: rgba(255, 255, 255, 0.4);
+  height: 50px;
+  width: 45%;
+  margin: 10px 2%;
+  border: 2px dashed rgba(128, 203, 196, 0.4);
+  border-radius: 10px;
+  text-align: center;
+  float: left;
+  color: rgba(128, 203, 196, 0.4);
+}
+.bulletin1 {
+  background: #ffffff;
+  height: 50px;
+  width: 45%;
+  margin: 10px 2%;
+  border: 2px dashed red;
+  border-radius: 10px;
+  text-align: center;
+  float: left;
+  font-weight: bold;
+}
+.bulletin2 {
+  background: #ffffff;
+  height: 50px;
+  width: 45%;
+  margin: 10px 2%;
+  border: 2px dashed #80cbc4;
+  border-radius: 10px;
+  text-align: center;
+  float: left;
+}
+.bus-title {
+  font-size: 15px;
+  font-weight: bold;
+}
+.bus-text {
+  line-height: 50px;
+  font-size: 13px;
 }
 .error-text {
   margin-top: 10px;
